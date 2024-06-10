019cd7d6d1c612b49244eb6d85434c87e1dfe291
Thivyasree-Rajaraman <thivyasree.ktr@shopup.org>
2024-06-10 10:27:34 +0530

    feat(login): add login components and utils

---
+++ src/pages/login/Login/store/LoginTypes.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export interface GoogleLoginButtonProps {
+    onSuccess: (user: { name: string }) => void;
+    onFailure: (error: any) => void;
+    className?: string;
+}
+
+export interface GoogleCodeResponse {
+    code: string;
+}
+
+export interface User {
+    name: string;
+}
+
+export interface Error {
+}
+
+export type currentUserType = {
+    user_id: number;
+    name: string;
+    email: string;
+    isLoggedIn: boolean
+}
+
